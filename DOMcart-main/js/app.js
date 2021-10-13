3d9cbf678eb20bbe71dae259697a4ef2691de9eb
RJGATON007 <robertgaton2017@gmail.com>
2021-10-13 22:34:00 +0800

    nag add ng functions, delegation, creating object with course data etc.

--- DOMcart-main/js/app.js
+++ DOMcart-main/js/app.js
@@ -21,3 +21,52 @@ function loadEventListeners() {
     // document ready
     document.addEventListener('DOMContentLoaded', getFromLocalStorage);
 }
+// functions
+function buyCourse(e) {
+    e.preventDefault();
+    // use delegation to find the course that was added
+    if(e.target.classList.contains('add-to-cart')) {
+        // read the course values
+        const course = e.target.parentElement.parentElement;
+        
+        // read the values
+        getCourseInfo(course);
+    }
+}
+// reads the HTML information of the selected course
+function getCourseInfo(course) {
+    // create an object with course data
+    const courseInfo = {
+        image: course.querySelector('img').src,
+        title: course.querySelector('h4').textContent,
+        price: course.querySelector('.price span').textContent,
+        id: course.querySelector('a').getAttribute('data-id')   
+    }
+    // insert ito the shopping cart
+    addIntoCart(courseInfo);
+}
+// display the selected course in to the shopping cart
+
+function addIntoCart(course) {
+    // create a <tr>
+    const row = document.createElement('tr');
+
+    // Build the template
+    row.innerHTML = `
+        <tr>
+            <td>
+                 <img src="${course.image}" width=100>
+            </td>
+            <td>${course.title}</td>
+            <td>${course.price}</td>
+            <td>
+                 <a href="#" class="remove" data-id="${course.id}">X</a>
+            </td>
+        </tr>
+    `;
+     // add into the shopping cart
+     shoppingCartContent.appendChild(row);
+
+     // add course into storage
+     saveIntoStorage(course);
+}
