45b059f815312d534aaf6d69ff4032bc4c05104e
RJGATON007 <robertgaton2017@gmail.com>
2021-10-13 22:29:34 +0800

    added variables

---
+++ DOMcart-main/js/app.js
@@ -0,0 +1,6 @@
+// variables
+const courses = document.querySelector('#courses-list'),
+      shoppingCartContent = document.querySelector('#cart-content tbody'),
+      clearCartBtn = document.querySelector('#clear-cart');
+
+
